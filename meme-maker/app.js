c303a3ed3b51f098b4d61130ed35e92cba930c2a
sunmingLee <25thbam@gmail.com>
2023-03-02 18:26:36 +0900

    Practicing Canvas
    노마드 코더 - 바닐라 JS로 그림 앱 만들기
    #0.0 ~ #2.1

---
+++ meme-maker/app.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector("canvas");
+
+const ctx = canvas.getContext("2d"); // 캔버스에 그림을 그리기 위한 붓
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.lineWidth = 2;
+
+let isPainting = false;
+
+function onMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+    isPainting = true;
+}
+
+function cancelPainting() {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
